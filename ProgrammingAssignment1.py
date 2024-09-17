7938250eb7fc5753fb0afbcf98786452b3638169
LilKittenKate <89802360+LilKittenKate@users.noreply.github.com>
2024-09-16 22:10:33 -0500

    Add files via upload

--- ProgrammingAssignment1.py
+++ ProgrammingAssignment1.py
@@ -24,31 +24,56 @@ def binary_search(A:list, x:int)->bool:
 
     return False
 
-def test_sorted_sequences()->dict:
-    runtimes = {}
+def test_sorted_sequences(length:list,lin:list,bi:list): #->dict:
+    #runtimes = {}
+    #length= []
+    i:int = 1
     with open("sequences_output.txt", "a") as outfile:
         with open("sorted_sequences.txt", "r") as file:
             for line in file:
+                length.append(i)
                 line = [int(x) for x in line.split()]
                 lin_start_time = time.time()
                 lin_result = linear_search(line, 500)
                 lin_end_time = time.time()
+                lin.append(lin_end_time-lin_start_time)
                 bin_start_time = time.time()
                 bin_result = binary_search(line, 500)
                 bin_end_time = time.time()
-                outfile.write(str(lin_result) + " " + str(bin_result) + "\n")
-                runtimes[len(line)] = (lin_end_time-lin_start_time, bin_end_time-bin_start_time)
-    return runtimes
+                bi.append(bin_end_time-bin_start_time)
+                outfile.write( str(lin_result) + " " + str(bin_result) + "\n")
+                #runtimes[len(line)] = (lin_end_time-lin_start_time, bin_end_time-bin_start_time)
+                i = i+1
+        #outfile.write(str(length))
+    #return runtimes
+    #return length
     
 def plot_times():
-    runtimes = test_sorted_sequences()
-    x = np.array(runtimes.keys())
-    y = np.array(runtimes.values())
-    plt.plot(x, y)
+    #lin:list
+    #bi:list
+    length = []
+    lin = []
+    bi = []
+    test_sorted_sequences(length,lin,bi)
+    #length = test_sorted_sequences()
+    #x = np.array(runtimes.keys())
+    x=[1,2,3]
+    #y = np.array(runtimes.values())
+    y=[1,4,9]
+    with open("sequences_output.txt", "a") as outfile:
+         outfile.write(str(length))
+    #plt.plot(x, y)
+    #plt.plot(length,lin)
+    plt.plot(length,lin,'r--',length,bi,'b--')
+    plt.xlabel('array length')
+    plt.ylabel('time (s)')
+    plt.title('Search time')
+    plt.grid()
+    plt.savefig("search_time.png")
     plt.show()
+    
             
 plot_times()
-
 
 
  
